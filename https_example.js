fbb321ec6c0b9f49aa1ad8fa3e3f2fc7cbff8947
Socha17 <mattsocha11@gmail.com>
2017-04-09 02:23:52 +0000

    just added the most lit http.request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,21 @@
 var https = require('https');
 
 console.log('I did it!');
+
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+}
+
+
+var callBack = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("im about to make the request");
+
+
+https.request(options, callBack).end();
+
+console.log("ive made the request");
