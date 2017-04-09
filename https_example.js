9512ade3c862a179b7c74ed9232daf4f483770f4
Socha17 <mattsocha11@gmail.com>
2017-04-09 02:51:45 +0000

    filtered response to show html

--- https_example.js
+++ https_example.js
@@ -10,14 +10,15 @@ var options = {
 
 
 var callBack = function(response) {
-
   console.log('In response handler callback!');
-  console.log("Response", response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + '--]');
+    console.log(chunk.toString());
+  })
+
 }
 
 console.log("im about to make the request");
-
-
 https.request(options, callBack).end();
-
 console.log("ive made the request");
